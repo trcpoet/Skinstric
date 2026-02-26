f61bc845d38dc11b99f6f47c5cd3347177989e76
Tahsin <tchowdhury29@gmail.com>
2026-02-25 22:06:10 -0600

    Fixed take test button hover affect

--- components/landing/HeroSideControl.tsx
+++ components/landing/HeroSideControl.tsx
@@ -19,30 +19,33 @@ const HeroSideControl = forwardRef<HTMLDivElement, HeroSideControlProps>(
         ref={ref}
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
+        onClick={onClick}
         className={cn(
-          "flex items-center gap-4",
+          "group flex items-center gap-4 cursor-pointer",
           side === "right" && "flex-row-reverse",
           className
         )}
       >
         <button
-          onClick={onClick}
-          className="group transition-opacity hover:opacity-80 flex items-center justify-center outline-none"
+          className="relative flex items-center justify-center outline-none transition-transform duration-500 ease-[cubic-bezier(0.34,1.56,0.64,1)] group-hover:scale-[0.8]"
           aria-label={label}
         >
           {side === "left" ? (
             <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M43.293 22L22 43.293L0.707031 22L22 0.707031L43.293 22Z" stroke="#1A1B1C" className="transition-colors group-hover:fill-[#1A1B1C]"/>
-              <path d="M15.7144 22L25.1429 27.4436V16.5564L15.7144 22Z" fill="#1A1B1C" className="transition-colors group-hover:fill-white"/>
+              <path d="M43.293 22L22 43.293L0.707031 22L22 0.707031L43.293 22Z" stroke="#1A1B1C" className="transition-colors duration-500 ease-out group-hover:fill-[#1A1B1C]"/>
+              <path d="M15.7144 22L25.1429 27.4436V16.5564L15.7144 22Z" fill="#1A1B1C" className="transition-all duration-500 ease-[cubic-bezier(0.34,1.56,0.64,1)] group-hover:fill-white group-hover:-translate-x-1"/>
             </svg>
           ) : (
             <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M43.293 22L22 43.293L0.707031 22L22 0.707031L43.293 22Z" stroke="#1A1B1C" className="transition-colors group-hover:fill-[#1A1B1C]"/>
-              <path d="M28.1426 22L18.714 27.4436V16.5564L28.1426 22Z" fill="#1A1B1C" className="transition-colors group-hover:fill-white"/>
+              <path d="M43.293 22L22 43.293L0.707031 22L22 0.707031L43.293 22Z" stroke="#1A1B1C" className="transition-colors duration-500 ease-out group-hover:fill-[#1A1B1C]"/>
+              <path d="M28.1426 22L18.714 27.4436V16.5564L28.1426 22Z" fill="#1A1B1C" className="transition-all duration-500 ease-[cubic-bezier(0.34,1.56,0.64,1)] group-hover:fill-white group-hover:translate-x-1"/>
             </svg>
           )}
         </button>
-        <span className="text-sm font-bold tracking-wide text-[#1A1B1C]">
+        <span className={cn(
+          "text-sm font-bold tracking-wide text-[#1A1B1C] transition-transform duration-500 ease-[cubic-bezier(0.34,1.56,0.64,1)]",
+          side === "left" ? "group-hover:translate-x-1" : "group-hover:-translate-x-1"
+        )}>
           {label}
         </span>
       </div>
